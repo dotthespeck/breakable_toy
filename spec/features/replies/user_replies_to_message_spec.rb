3bc8b001beb23804342dfb4e8cc5ac83d1414a21
Zoe <doticus@gmail.com>
2015-01-17 22:21:13 -0500

    Replies in chronological order

--- spec/features/replies/user_replies_to_message_spec.rb
+++ spec/features/replies/user_replies_to_message_spec.rb
@@ -7,7 +7,7 @@ feature 'User replies to a message', %Q{
   } do
     # Acceptance Criteria
     # [x] I can create a reply to a message in the post thread
-    # [] I can see my message in the thread in chronological order
+    # [x] I can see my message in the thread in chronological order
 
     scenario 'User replies to a message' do
       user = FactoryGirl.create(:user)
@@ -22,5 +22,29 @@ feature 'User replies to a message', %Q{
       click_button 'Submit'
 
       expect(page).to have_content "New reply"
+    end
+
+    scenario 'User sees posts in chronological order' do
+      user = FactoryGirl.create(:user)
+      conversation = FactoryGirl.create(:conversation)
+      message1 = FactoryGirl.create(:message, user: user, conversation: conversation, created_at: Time.now - 3.days)
+      message2 = FactoryGirl.create(:message, post: "Middle post", user: user, conversation: conversation, parent_id: message1.id, created_at: Time.now - 2.days)
+      message3 = FactoryGirl.create(:message, post: "Most recent post", user: user, conversation: conversation, parent_id: message1.id, created_at: Time.now - 1.days)
+
+      sign_in_as(user)
+
+      visit conversation_path(conversation)
+      click_link 'Reply to post'
+
+      fill_in 'Post', with: message2.post
+      click_button 'Submit'
+
+      visit conversation_path(conversation)
+      click_link 'Reply to post'
+
+      fill_in 'Post', with: message3.post
+      click_button 'Submit'
+
+      expect(message2.post).to appear_before message3.post
     end
   end
